9949b81261678265409586c62da29263521301e9
AndresCastil <casaned@yahoo.com>
2022-10-02 11:46:41 +0200

    Primer ejercicio

---
+++ UD02/ComentarioLinea.java
@@ -0,0 +1,7 @@
+public class ComentarioLinea{
+	public static void main(String args[]){
+		//System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("FIN DE PROGRAMA");
+	}
+}
